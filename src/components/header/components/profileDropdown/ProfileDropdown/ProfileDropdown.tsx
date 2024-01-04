0bd74d8c989ee1f051604d6f8246a77f6e809ae0
Yin Yue <prinostaged@gmail.com>
2024-01-04 10:17:08 +0530

    profile username

--- src/components/header/components/profileDropdown/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/header/components/profileDropdown/ProfileDropdown/ProfileDropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProfileOverlay } from '../ProfileOverlay/ProfileOverlay';
 import { useAppSelector } from '@app/hooks/reduxHooks';
 import { useResponsive } from '@app/hooks/useResponsive';
@@ -7,13 +7,24 @@ import { BasePopover } from '@app/components/common/BasePopover/BasePopover';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseAvatar } from '@app/components/common/BaseAvatar/BaseAvatar';
+import { fetchUserAndToken } from '@app/api/getUserAndToken';
 
 export const ProfileDropdown: React.FC = () => {
   const { isTablet } = useResponsive();
-
+  const [emailName, setEmailName] = useState<string>('');
   const user = useAppSelector((state) => state.user.user);
 
-  return user ? (
+  useEffect(() => {
+    const getUserEmail = async () => {
+      const userData = await fetchUserAndToken();
+      const emailPart = userData.email.split('@')[0]; // Split the email and take the first part
+      setEmailName(emailPart);
+    };
+
+    getUserEmail();
+  }, []);
+
+  return emailName && user ? (
     <BasePopover content={<ProfileOverlay />} trigger="click">
       <S.ProfileDropdownHeader as={BaseRow} gutter={[10, 10]} align="middle">
         <BaseCol>
@@ -21,7 +32,7 @@ export const ProfileDropdown: React.FC = () => {
         </BaseCol>
         {isTablet && (
           <BaseCol>
-            <span>{`${user.firstName} ${user.lastName[0]}`}</span>
+            <span>{`${emailName}`}</span> {/* Use emailName here */}
           </BaseCol>
         )}
       </S.ProfileDropdownHeader>
