939ca72225b86cc7260eab079d27cdc1e9a72438
Yin Yue <prinostaged@gmail.com>
2024-01-04 10:00:43 +0530

    prettier-deployment-error

--- src/environment.ts
+++ src/environment.ts
@@ -1,3 +1,4 @@
 export const THOUGHTSPOT_HOST = process.env.REACT_APP_TS_URL || process.env.REACT_APP_THOUGHTSPOT_HOST || '';
 export const LIVEBOARD_ID = '1d8000d8-6225-4202-b56c-786fd73f95ad' || '';
 export const SECRET_KEY = process.env.REACT_APP_SECRET_KEY || '';
+export const USERNAME = process.env.REACT_APP_USERNAME || '';
